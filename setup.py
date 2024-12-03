b287bebc24b4cd827eafd9eb9d219f1254dd3eb1
CJY-C <masarity@outlook.com>
2024-12-03 19:13:30 +0800

    build(setup): add setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,20 @@
-/home/ck/projekte/python_open_source/extern_rawlings_mpctools_py3/mpctoolssetup.py
+"""See README for description."""
+from distutils.core import setup
+# import mpctools
+
+setup(name="MPCTools",
+    # dependencies
+    install_requires=[
+      "casadi==3.6.0",
+      "numpy==1.23.5",
+      ],
+    version='2.4.2',
+    description="Nonlinear MPC tools for use with CasADi",
+    author="Michael Risbeck",
+    author_email="risbeck@wisc.edu",
+    url="https://bitbucket.org/rawlings-group/mpc-tools-casadi",
+    long_description=__doc__,
+    packages=["mpctools"],
+    platforms=["N/A"],
+    license="GNU LGPLv3",
+)
